1353406fa2505d57acabba69293217cd37a76ea8
l_rad <0mega.lolguy@mail.ru>
2025-08-24 11:39:00 +0300

    Initial commit

---
+++ router.py
@@ -0,0 +1,25 @@
+from typing import Annotated
+
+from fastapi import APIRouter, Depends
+
+from repository import TaskRepository
+from schemas import STaskAdd, STask, STaskID
+
+router = APIRouter(
+    prefix="/tasks",
+    tags=["Таски"]
+)
+
+
+@router.post("")
+async def add_task(
+    task: Annotated[STaskAdd, Depends()]  # Чтобы сделать заполнение полей, а не json'a
+) -> STaskID:
+    task_id = await TaskRepository.add_one(task)
+    return {"ok": True, "task_id": task_id}
+
+
+@router.get("")
+async def get_tasks() -> list[STask]:
+    tasks = await TaskRepository.find_all()
+    return tasks
